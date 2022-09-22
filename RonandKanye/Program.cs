ee3dce54a516e12871341bd7406a595016d495e9
Derek Griffin <griffinderek6@gmail.com>
2022-09-22 13:37:12 -0600

    finished'
    git commit -m'finished'

---
+++ RonandKanye/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+for {
+    var client = new HttpClient();
+    //Kanye
+    var kanyeURL = "https://api.kanye.rest";
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+    var kanyeQoute = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+    Console.WriteLine(kanyeResponse);
+
+    //Ron
+    var ronURL = " https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+    var ronResponse = client.GetStringAsync(ronURL).Result;
+    var ronObject = JArray.Parse(ronResponse);
+    Console.WriteLine($"Ron: {ronObject[0]}");
+    Console.WriteLine();
+    }
